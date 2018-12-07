926886b4ede0f4c4a0e7eda96cffb94b2e461c03
Timo Grossenbacher <wnstnsmth@yahoo.com>
2018-12-07 11:34:54 +0100

    patched to new rddj-template

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 # knit
-# add export PATH="$PATH:/usr/lib/rstudio/bin/pandoc" to your PATH in order for the following to work
+source knit.sh
 # make temporary copy of preprocessing folder with all data we need in build
 cp -r analysis tmp
 mv deploy.sh tmp/
 # switch to gh-pages branch
 git checkout gh-pages
+if [ $? -eq 0 ]
+then
+  echo "changed to gh-pages"
+else
+  # revert
+  mv tmp/deploy.sh deploy.sh
+  rm -rf tmp/
+  exit 1
+fi
 # copy over index file (the processed main.Rmd) from master branch
 cp tmp/main.html index.html
 # clean
@@ -19,8 +28,6 @@ cp -r tmp/output rscript/
 # copy over other necessary input files from master branch
 cp -r tmp/input rscript/
 cp -r tmp/scripts rscript/
-cp -r tmp/images rscript/
-cp tmp/main.pdf rscript/
 # zip the rscript folder
 zip -r rscript.zip rscript
 # remove the rscript folder
